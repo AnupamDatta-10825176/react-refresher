3b90dc7f31430b46f3b3f4aebaf5a6e8121717a8
AnupamDatta-10825176 <anupam.datta@ltimindtree.com>
2025-01-06 19:16:27 +0530

    add control for select element

--- travel-list-app/src/components/Form/index.js
+++ travel-list-app/src/components/Form/index.js
@@ -1,22 +1,40 @@
+import { useState } from "react";
 import "./Form.css";
 
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("submitted");
+    console.log(`Input: ${description}`);
+    console.log(`Select: ${quantity}`);
+  };
+
+  const handleOnchangeQuantity = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const handleOnchangeItem = (event) => {
+    setDescription(event.target.value);
   };
 
   return (
     <form className="add-form" onSubmit={(event) => handleSubmit(event)}>
       <h3>What do you need for your Trip? 😍</h3>
-      <select>
+      <select value={quantity} onChange={(e) => handleOnchangeQuantity(e)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(event) => handleOnchangeItem(event)}
+      />
       <button type="submit">add</button>
     </form>
   );
