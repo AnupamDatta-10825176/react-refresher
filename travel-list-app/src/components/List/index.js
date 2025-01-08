8f4fa6ea9e4ef97a564c0111fa1c5c38e5a5622b
AnupamDatta-10825176 <anupam.datta@ltimindtree.com>
2025-01-08 16:17:13 +0530

    add sorting logic

--- travel-list-app/src/components/List/index.js
+++ travel-list-app/src/components/List/index.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import Item from "./Item";
 import "./List.css";
 
 const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems = [];
+  switch (sortBy) {
+    case "input":
+      sortedItems = items;
+      break;
+    case "description":
+      sortedItems = items
+        .slice()
+        .sort((a, b) => a.description.localeCompare(b.description));
+      break;
+    case "packed":
+      sortedItems = items
+        .slice()
+        .sort((a, b) => Number(a.packed) - Number(b.packed));
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -14,6 +36,14 @@ const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by Input order</option>
+          <option value="description">Sort by Description</option>
+          <option value="packed">Sort by Packed Status</option>
+        </select>
+      </div>
     </div>
   );
 };
