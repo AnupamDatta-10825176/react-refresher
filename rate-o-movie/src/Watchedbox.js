6c1a0e961f4e5ec02f29377e972d05742e387401
AnupamDatta-10825176 <anupam.datta@ltimindtree.com>
2025-01-09 22:39:35 +0530

    create WatchedList component

--- rate-o-movie/src/Watchedbox.js
+++ rate-o-movie/src/Watchedbox.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import WatchedSummery from "./WatchedSummery";
+import WatchedList from "./WatchedList";
 import { tempWatchedData } from "./data";
 
 const WatchedBox = () => {
@@ -18,29 +19,7 @@ const WatchedBox = () => {
       {isOpen2 && (
         <>
           <WatchedSummery watched={watched} />
-
-          <ul className="list">
-            {watched.map((movie) => (
-              <li key={movie.imdbID}>
-                <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                <h3>{movie.Title}</h3>
-                <div>
-                  <p>
-                    <span>⭐️</span>
-                    <span>{movie.imdbRating}</span>
-                  </p>
-                  <p>
-                    <span>🌟</span>
-                    <span>{movie.userRating}</span>
-                  </p>
-                  <p>
-                    <span>⏳</span>
-                    <span>{movie.runtime} min</span>
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <WatchedList watched={watched} />
         </>
       )}
     </div>
