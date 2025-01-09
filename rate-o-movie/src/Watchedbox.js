b6e8c356887b4790fc70c97b7f165a2e0be306f8
AnupamDatta-10825176 <anupam.datta@ltimindtree.com>
2025-01-09 22:37:53 +0530

    create WatchedSummery component

--- rate-o-movie/src/Watchedbox.js
+++ rate-o-movie/src/Watchedbox.js
@@ -1,16 +1,11 @@
 import { useState } from "react";
+
+import WatchedSummery from "./WatchedSummery";
 import { tempWatchedData } from "./data";
 
 const WatchedBox = () => {
   const [isOpen2, setIsOpen2] = useState(true);
   const [watched, setWatched] = useState(tempWatchedData);
-
-  const average = (arr) =>
-    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
 
   return (
     <div className="box">
@@ -22,27 +17,7 @@ const WatchedBox = () => {
       </button>
       {isOpen2 && (
         <>
-          <div className="summary">
-            <h2>Movies you watched</h2>
-            <div>
-              <p>
-                <span>#️⃣</span>
-                <span>{watched.length} movies</span>
-              </p>
-              <p>
-                <span>⭐️</span>
-                <span>{avgImdbRating}</span>
-              </p>
-              <p>
-                <span>🌟</span>
-                <span>{avgUserRating}</span>
-              </p>
-              <p>
-                <span>⏳</span>
-                <span>{avgRuntime} min</span>
-              </p>
-            </div>
-          </div>
+          <WatchedSummery watched={watched} />
 
           <ul className="list">
             {watched.map((movie) => (
