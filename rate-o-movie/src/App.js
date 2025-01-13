8835c796c0cd956a898e242eafb62b0c3c358324
AnupamDatta-10825176 <anupam.datta@ltimindtree.com>
2025-01-13 18:18:12 +0530

    add loading component

--- rate-o-movie/src/App.js
+++ rate-o-movie/src/App.js
@@ -11,6 +11,7 @@ import WatchedBox from "./Watchedbox";
 import MovieList from "./MovieList";
 import WatchedSummery from "./WatchedSummery";
 import WatchedMoviesList from "./WatchedList";
+import Loading from "./Loading";
 
 // data and constants
 import { tempWatchedData } from "./data";
@@ -19,12 +20,21 @@ import { movieApiURL } from "./constants";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    // start showing loading message
+    setIsLoading(true);
+
+    // fetch data from external api
     async function fetchData() {
       const res = await fetch(`${movieApiURL}s=gravity`);
       const resJson = await res.json();
+      // update movies with data fetched based on query.
       setMovies(resJson.Search);
+
+      // end showing loading message
+      setIsLoading(false);
     }
     fetchData();
   }, []);
@@ -37,7 +47,7 @@ export default function App() {
       </Navbar>
       <Main>
         <ListBox>
-          <MovieList movies={movies} />
+          {isLoading ? <Loading /> : <MovieList movies={movies} />}
         </ListBox>
         <WatchedBox>
           <WatchedSummery watched={watched} />
---
+++ rate-o-movie/src/Loading.js
@@ -0,0 +1,16 @@
+const styleLoading = {
+  fontSize: "2.4rem",
+  textAlign: "center",
+  fontWeight: "bold",
+  margin: "4.8rem",
+};
+
+const Loading = () => {
+  return (
+    <div style={styleLoading}>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
