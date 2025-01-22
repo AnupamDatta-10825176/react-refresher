919f5c9c91d73e68510053880b4f1d9e8236d4f7
AnupamDatta-10825176 <anupam.datta@ltimindtree.com>
2025-01-22 13:12:31 +0530

    complete implementation

--- geo-location/src/App.js
+++ geo-location/src/App.js
@@ -1,5 +1,62 @@
+import { useState } from "react";
+
+const styleButton = { cursor: "pointer" };
+
 const App = () => {
-  return <div>Initial Project</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [countClicks, setCountClicks] = useState(0);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  const { lat, lng } = position;
+
+  function getPosition() {
+    setCountClicks((prevCount) => prevCount + 1);
+
+    if (!navigator.geolocation) {
+      return setError("Your browser does not support geolocation");
+    }
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (err) => {
+        setError(err.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return (
+    <div>
+      <button style={styleButton} onClick={getPosition} disabled={isLoading}>
+        Get my Position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
 };
 
 export default App;
